6f3753f077814a7a27fdd8e6c0bbb17aee569b31
Karen0731 <karen.s0731s@gmail.com>
2019-02-05 23:32:34 +0900

    initial commit

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+  has_many :tweets
+  has_many :comments
+  #userに対してtweetは多なのでhas_many
+  #userに対してcommentは多なのでhas_many
+end
